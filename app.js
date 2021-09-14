88bf789c3cd4d34ddfd2be529afaabf5b696c771
irfanfauzi <irfanfauzi24@yahoo.com>
2021-09-14 22:02:32 +0700

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+
+
+
+
+
+
+
+
+
+
+// Create a Server
+app.listen(PORT, () => {
+  console.log(`aplikasi sedang berjalan di : http://localhost:${PORT}`)
+})
