7c5e3412a2fca29305a4c0de1209c414ffd2110e
irfanfauzi <irfanfauzi24@yahoo.com>
2021-09-15 14:19:51 +0700

    cors

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ const app = express()
 const PORT = 3000
 const productRoutes = require('./src/routes/product')
 
-// router
+// rules / aturan jika komputer lain / url selain localhost:3000 mengakses data ini
+app.use((req, res, next) => {
 
+  // setHeader(izinkan akses url end-poin, url apa saja yang boleh akses?)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // setHeader(izinkan akses Method, method apa saja yang boleh di akses oleh url lain?)
+  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE, OPTIONS')
+  // tipe konten JSON
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
+})
 
 
 app.use('/', productRoutes)
